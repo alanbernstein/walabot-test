714a1f8b6eab6c69995840477bd0419db15cda35
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-01-02 23:27:27 -0600

    update stuff

--- wall-scanner.py
+++ wall-scanner.py
@@ -3,18 +3,23 @@ from sys import platform
 from os import system
 import WalabotAPI as wlbt
 import matplotlib.pyplot as plt
-import numpy as np
-
-
-def print_target_short(t):
-    print('Target #{}\nx = {}\ny = {}\nz = {}\n'.format(
-        t.xPosCm, t.yPosCm, t.zPosCm))
 
 
 def print_target(t):
     pos = '(%6.4f, %6.4f, %6.4f) cm' % (t.xPosCm, t.yPosCm, t.zPosCm)
-    print('%s, width = %6.4f cm, strength = %f' % (pos, t.widthCm, t.amplitude))
-    print('  type=%s, angle=%f deg' % (t.type, t.angleDeg))
+    print('%s, strength = %f' % (pos, t.amplitude))
+
+    if hasattr(t, 'angleDeg'):
+        # how to get this?
+        print('  width = %6.4f cm, type=%s, angle=%f deg' % (t.widthCm, t.type, t.angleDeg))
+
+
+profiles = {
+    'sensor': wlbt.PROF_SENSOR,
+    'imaging': wlbt.PROF_SHORT_RANGE_IMAGING,
+}
+
+profile = 'imaging'
 
 
 def main():
@@ -22,8 +27,10 @@ def main():
     wlbt.SetSettingsFolder()  # set the path to the essetial database files
     wlbt.ConnectAny()  # establishes communication with the Walabot
 
-    wlbt.SetProfile(wlbt.PROF_SENSOR)  # set scan profile out of the possibilities
+    wlbt.SetProfile(profiles[profile])  # set scan profile out of the possibilities
     wlbt.SetDynamicImageFilter(wlbt.FILTER_TYPE_MTI)  # specify filter to use
+
+    # TODO: calibrate
 
     wlbt.Start()  # starts Walabot in preparation for scanning
 
@@ -34,7 +41,12 @@ def main():
 
     while True:
         wlbt.Trigger()  # initiates a scan and records signals
-        targets = wlbt.GetSensorTargets()  # provides a list of identified targets
+        if profile == 'sensor':
+            targets = wlbt.GetSensorTargets()  # provides a list of identified targets
+        elif profile == 'imaging':
+            targets = wlbt.GetImagingTargets()  # only one target
+        # raw_image = wlbt.GetRawImageSlice()[0] # is there anything else in this iterable?
+        # dim = wlbt.getRawImageSliceDimensions()
 
         # update terminal
         system('cls' if platform == 'win32' else 'clear')  # clear the terminal
@@ -45,14 +57,14 @@ def main():
         for n, t in enumerate(targets):
             handles[n].set_xdata(t.xPosCm)
             handles[n].set_ydata(t.yPosCm)
-            handles[n].set_linestyle('ko')
-            handles[n].set_markersize(t.amplitude
+            handles[n].set_marker('o')
+            handles[n].set_color('k')
+            handles[n].set_markersize(t.amplitude)
 
+        plt.xlim(-150, 150)
+        plt.ylim(-150, 150)
         plt.draw()
         plt.pause(0.01)
-        
-
-
 
     wlbt.Stop()  # stops Walabot when finished scanning
     wlbt.Disconnect()  # stops communication with Walabot
